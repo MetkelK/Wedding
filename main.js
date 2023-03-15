ccc8a30442f50c7b92a01239a980f056a007fc28
Metkel K <metkelk@gmail.com>
2023-03-15 18:16:19 -0400

    test

--- main.js
+++ main.js
@@ -1,40 +1,82 @@
-// Get DOM elements
-const attendSelect = document.getElementById("attend");
-const dinnerSection = document.getElementById("dinner-section");
-const bringGuestsSelect = document.getElementById("bring-guests");
-const guestSection = document.getElementById("guest-section");
-const numGuestsSelect = document.getElementById("num-guests");
-const guestNamesDiv = document.getElementById("guest-names");
-const guestNamesSection = document.getElementById("guest-names-section");
-const songRequestsSection = document.getElementById("song-requests-section");
-const transportationSection = document.getElementById("transportation-section");
-const questionsSection = document.getElementById("questions-section");
-const guestRadioYes = document.getElementById("guest-yes");
-const guestDetails = document.getElementById("guest-details");
+// Get references to all the necessary DOM elements
+const attendingRadio = document.querySelector('input[name="attendance"]');
+const guestRadio = document.querySelector('input[name="guest"]');
+const guestDetails = document.querySelector("#guest-info");
+const dinnerDetails = document.querySelector("#dinner-options");
+// const transportDetails = document.querySelector("#transport-details");
 
-// Show/hide sections based on user input
-attendSelect.addEventListener("change", function () {
-  if (this.value === "yes") {
-    dinnerSection.classList.remove("hidden");
-    songRequestsSection.classList.remove("hidden");
-    transportationSection.classList.remove("hidden");
-    questionsSection.classList.remove("hidden");
+// Add event listeners to the attendingRadio element
+attendingRadio.addEventListener("change", function () {
+  if (attendingRadio.value === "yes") {
+    console.log(attendingRadio);
+    // Show the dinner details section
+    dinnerDetails.classList.remove("hidden");
   } else {
-    dinnerSection.classList.add("hidden");
-    bringGuestsSelect.value = "";
-    guestSection.classList.add("hidden");
-    guestNamesDiv.innerHTML = "";
+    // Hide the dinner details section
+    console.log(attendingRadio);
+    dinnerDetails.classList.add("hidden");
   }
 });
 
-guestRadioYes.addEventListener("change", function () {
-  console.log("Radio button checked:", guestRadioYes.checked);
-  if (guestRadioYes.checked) {
+// Add event listeners to the guestRadio element
+guestRadio.addEventListener("change", function () {
+  if (guestRadio.value === "yes") {
+    // Show the guest details section
     guestDetails.classList.remove("hidden");
   } else {
+    // Hide the guest details section
     guestDetails.classList.add("hidden");
   }
 });
+
+// Add event listeners to the transportRadio element
+// transportRadio.addEventListener("change", function () {
+//   if (transportRadio.value === "yes") {
+//     // Show the transport details section
+//     transportDetails.classList.remove("hidden");
+//   } else {
+//     // Hide the transport details section
+//     transportDetails.classList.add("hidden");
+//   }
+// });
+
+// Get DOM elements
+// const attendSelect = document.getElementById("attend");
+// const dinnerSection = document.getElementById("dinner-section");
+// const bringGuestsSelect = document.getElementById("bring-guests");
+// const guestSection = document.getElementById("guest-section");
+// const numGuestsSelect = document.getElementById("num-guests");
+// const guestNamesDiv = document.getElementById("guest-names");
+// const guestNamesSection = document.getElementById("guest-names-section");
+// const songRequestsSection = document.getElementById("song-requests-section");
+// const transportationSection = document.getElementById("transportation-section");
+// const questionsSection = document.getElementById("questions-section");
+// const guestRadioYes = document.getElementById("guest-yes");
+// const guestDetails = document.getElementById("guest-details");
+
+// // Show/hide sections based on user input
+// attendSelect.addEventListener("change", function () {
+//   if (this.value === "yes") {
+//     dinnerSection.classList.remove("hidden");
+//     songRequestsSection.classList.remove("hidden");
+//     transportationSection.classList.remove("hidden");
+//     questionsSection.classList.remove("hidden");
+//   } else {
+//     dinnerSection.classList.add("hidden");
+//     bringGuestsSelect.value = "";
+//     guestSection.classList.add("hidden");
+//     guestNamesDiv.innerHTML = "";
+//   }
+// });
+
+// guestRadioYes.addEventListener("change", function () {
+//   console.log("Radio button checked:", guestRadioYes.checked);
+//   if (guestRadioYes.checked) {
+//     guestDetails.classList.remove("hidden");
+//   } else {
+//     guestDetails.classList.add("hidden");
+//   }
+// });
 
 // bringGuestsSelect.addEventListener("change", function () {
 //   if (this.value === "yes") {
