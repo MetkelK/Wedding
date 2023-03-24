058fbd575e18963f2d274aa43dfefaf7e06995f1
Metkel K <metkelk@gmail.com>
2023-03-23 22:54:50 -0400

    test

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // Get references to all the necessary DOM elements
 const attendingRadios = document.querySelectorAll('input[name="attendance"]');
+const dinnerSelections = document.querySelectorAll(
+  'input[name="dinner-selection"]'
+);
 const guestRadios = document.querySelectorAll('input[name="guest-attendance"]');
 const guestDetails = document.querySelector("#guest-info");
 const dinnerDetails = document.querySelector("#dinner-options");
@@ -10,9 +13,17 @@ attendingRadios.forEach(function (attendingRadio) {
     if (attendingRadio.value === "Yes") {
       // Show the dinner details section
       dinnerDetails.classList.remove("hidden");
+      dinnerSelections.forEach((dinner) => {
+        dinner.setAttribute("required", "");
+        console.log(dinner);
+      });
     } else {
       // Hide the dinner details section
       dinnerDetails.classList.add("hidden");
+      dinnerSelections.forEach((dinner) => {
+        dinner.removeAttribute("required", "");
+        console.log(dinner);
+      });
     }
     console.log(attendingRadio.value);
   });
