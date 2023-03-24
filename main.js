ce13ce858ddf7bd2fb2eb0a8d1076fb2d3032cc7
Metkel K <metkelk@gmail.com>
2023-03-24 00:20:52 -0400

    updates

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 // Get references to all the necessary DOM elements
 const attendingRadios = document.querySelectorAll('input[name="attendance"]');
+const dinnerDetails = document.querySelector("#dinner-options");
 const dinnerSelections = document.querySelectorAll(
   'input[name="dinner-selection"]'
 );
 const guestRadios = document.querySelectorAll('input[name="guest-attendance"]');
 const guestDetails = document.querySelector("#guest-info");
-const dinnerDetails = document.querySelector("#dinner-options");
+const guestName = document.querySelector("#guest-name");
+const guestDinner = document.querySelectorAll(
+  'input[name="guest-dinner-selection"]'
+);
+const transportation = document.querySelectorAll(
+  'input[name="transportation"]'
+);
 
 // Add event listeners to the attendingRadios elements
 attendingRadios.forEach(function (attendingRadio) {
@@ -15,17 +22,26 @@ attendingRadios.forEach(function (attendingRadio) {
       dinnerDetails.classList.remove("hidden");
       dinnerSelections.forEach((dinner) => {
         dinner.setAttribute("required", "");
-        console.log(dinner);
+      });
+      guestRadios.forEach((radio) => {
+        radio.setAttribute("required", "");
+      });
+      transportation.forEach((transport) => {
+        transport.setAttribute("required", "");
       });
     } else {
       // Hide the dinner details section
       dinnerDetails.classList.add("hidden");
       dinnerSelections.forEach((dinner) => {
         dinner.removeAttribute("required", "");
-        console.log(dinner);
+      });
+      guestRadios.forEach((radio) => {
+        radio.removeAttribute("required", "");
+      });
+      transportation.forEach((transport) => {
+        transport.removeAttribute("required", "");
       });
     }
-    console.log(attendingRadio.value);
   });
 });
 
@@ -35,10 +51,17 @@ guestRadios.forEach(function (guestRadio) {
     if (guestRadio.value === "Yes") {
       // Show the guest details section
       guestDetails.classList.remove("hidden");
+      guestName.setAttribute("required", "");
+      guestDinner.forEach((dinner) => {
+        dinner.setAttribute("required", "");
+      });
     } else {
       // Hide the guest details section
       guestDetails.classList.add("hidden");
+      guestName.removeAttribute("required", "");
+      guestDinner.forEach((dinner) => {
+        dinner.removeAttribute("required", "");
+      });
     }
-    console.log(guestRadio.value);
   });
 });
